a6d0c577384f6bfebca0f8bff001f290ae7da3b8
Spencer <spencer.peq@gmail.com>
2021-02-03 21:21:34 -0800

    Create Profiles model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to='avatars', default='avatar.png')
+    background = models.ImageField(upload_to='backgrounds', default='background.jpg')
+    following = models.ManyToManyField(User, related_name='following', blank=True)
+    bio = models.TextField(default='Empty Bio')
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.user)
